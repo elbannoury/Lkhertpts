dfdc754e49d5c9c205c2c055930f8a991cccf897
elbannoury <elbannoury@gmail.com>
2026-07-05 14:15:22 +0100

    Change Supabase key to secret key
    Updated Supabase key from publishable to secret key.

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -3,7 +3,7 @@ import { createClient } from '@supabase/supabase-js';
 
 // Initialize database client
 const supabaseUrl = 'https://vmeljeafghdcbmepstak.supabase.co';
-const supabaseKey = 'sb_publishable_d7MktzeV50r6NH4611MHfg_tvcNHGBK';
+const supabaseKey = 'sb_secret_zqc3Rfz0fxoA0GpAVYuoxA_DmHbxS4L';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 
