7921967596bca6f78256ca633876ed5f58cb24d8
elbannoury <elbannoury@gmail.com>
2026-07-05 15:13:53 +0100

    Update Supabase client URL and key

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,8 +2,8 @@ import { createClient } from '@supabase/supabase-js';
 
 
 // Initialize database client
-const supabaseUrl = 'https://vmeljeafghdcbmepstak.supabase.co';
-const supabaseKey = 'sb_secret_zqc3Rfz0fxoA0GpAVYuoxA_DmHbxS4L';
+const supabaseUrl = 'https://guknrxxiewkygmlkcagk.databasepad.com';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IjhlYTljYTEyLWFjYjMtNDE3Yi04NjE3LTgwYWExNjY3YjY5YiJ9.eyJwcm9qZWN0SWQiOiJndWtucnh4aWV3a3lnbWxrY2FnayIsInJvbGUiOiJhbm9uIiwiaWF0IjoxNzgyNjAzMjA1LCJleHAiOjIwOTc5NjMyMDUsImlzcyI6ImZhbW91cy5kYXRhYmFzZXBhZCIsImF1ZCI6ImZhbW91cy5jbGllbnRzIn0.4w47hYH4PNZj8ygquA3PghKe854o0OegbexXOE23ka8';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 
