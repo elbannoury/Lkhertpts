c1fddc00d35110eda130582392ea80f6b9f76f7e
elbannoury <elbannoury@gmail.com>
2026-07-06 20:31:58 +0100

    Update Supabase URL and anon key

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types/supabase';
 
-const supabaseUrl = 'https://fzdxsaxxpabrjzsutwrq.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ6ZHhzYXh4cGFicmp6c3V0d3JxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMzMTk1ODYsImV4cCI6MjA5ODg5NTU4Nn0.MIgxkrgXE0hb5wALtvX3WJZ07ngRB8fAbFzz-a5uOJs';
+const supabaseUrl = 'https://srzxzpwispudxldqjjah.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNyenh6cHdpc3B1ZHhsZHFqamFoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMzNTM4MDcsImV4cCI6MjA5ODkyOTgwN30.AAa9rSrdcxdgNCkU-Ab9BKqcahbL49KCMIgvl8B3a0s';
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
 
