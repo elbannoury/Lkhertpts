492ea8dbdfdd58f83ad945c51993e1a7e8e30262
elbannoury <elbannoury@gmail.com>
2026-07-06 01:24:46 +0100

    Update Supabase client URL and key

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,8 +2,8 @@ import { createClient } from '@supabase/supabase-js';
 
 
 // Initialize database client
-const supabaseUrl = 'https://guknrxxiewkygmlkcagk.databasepad.com';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IjhlYTljYTEyLWFjYjMtNDE3Yi04NjE3LTgwYWExNjY3YjY5YiJ9.eyJwcm9qZWN0SWQiOiJndWtucnh4aWV3a3lnbWxrY2FnayIsInJvbGUiOiJhbm9uIiwiaWF0IjoxNzgyNjAzMjA1LCJleHAiOjIwOTc5NjMyMDUsImlzcyI6ImZhbW91cy5kYXRhYmFzZXBhZCIsImF1ZCI6ImZhbW91cy5jbGllbnRzIn0.4w47hYH4PNZj8ygquA3PghKe854o0OegbexXOE23ka8';
+const supabaseUrl = 'https://iedarnneftukutsswpxt.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImllZGFybm5lZnR1a3V0c3N3cHh0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMyODAyODMsImV4cCI6MjA5ODg1NjI4M30.I2Wa0hJUEBlcWqmAR5-pZQoFIs5MeJoCFErmuizx7y4';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 
