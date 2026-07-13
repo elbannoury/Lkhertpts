2a359b37369220a7b4790b284dd6f65bb380dd69
elbannoury <elbannoury@gmail.com>
2026-07-13 21:04:46 +0100

    Add service worker registration in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,14 @@
-
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
 // Remove dark mode class addition
 createRoot(document.getElementById("root")!).render(<App />);
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js').catch((err) => {
+      console.error('Service worker registration failed:', err);
+    });
+  });
+}
